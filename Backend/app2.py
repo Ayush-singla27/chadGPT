e48c9286e0bd2df01625432b039b7271326b81a1
Ayush Singla <77923967+Ayush-singla27@users.noreply.github.com>
2023-07-03 18:41:59 +0530

    Update app2.py

--- Backend/app2.py
+++ Backend/app2.py
@@ -123,5 +123,5 @@ def askqns():
 
 
 if __name__ == '__main__':
-    serve(app,host='https://backendchadgpt.onrender.com' , port=80 , threads =2)
+    serve(app,host='0.0.0.0' , port=80 , threads =2)
     # app.run(host='localhost', port=5000) 
