9eeba13e185612865a33f646c207e0ccf7d35825
Ayush Singla <77923967+Ayush-singla27@users.noreply.github.com>
2023-07-03 18:29:48 +0530

    Update app2.py

--- Backend/app2.py
+++ Backend/app2.py
@@ -123,5 +123,5 @@ def askqns():
 
 
 if __name__ == '__main__':
-    serve(app,host='0.0.0.0' , port=5000 , threads =2)
+    serve(app,host='13.228.225.19' , port=80 , threads =2)
     # app.run(host='localhost', port=5000) 
